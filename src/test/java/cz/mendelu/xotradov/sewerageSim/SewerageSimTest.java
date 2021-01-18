9349b6fe314ed7fcb3dffab95b85d2b2e931bbf8
otradovec <otradovec@github.com>
2021-01-18 10:14:24 +0100

    runFor() test

--- src/test/java/cz/mendelu/xotradov/sewerageSim/SewerageSimTest.java
+++ src/test/java/cz/mendelu/xotradov/sewerageSim/SewerageSimTest.java
@@ -1,9 +1,25 @@
 package cz.mendelu.xotradov.sewerageSim;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SewerageSimTest {
+    SewerageSim sewerageSim;
+    @Before
+    public void setUp() throws Exception {
+        sewerageSim = new SewerageSim();
+    }
 
+    @Test
+    public void runFor() {
+        SewerageSys system = new SewerageSys();
+        sewerageSim.createStaticSystem(system);
+        try {
+            sewerageSim.runFor(20,system);
+        }catch (RuntimeException e) {
+            fail();
+        }
+    }
 }
